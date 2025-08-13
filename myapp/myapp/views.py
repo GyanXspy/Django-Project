95fcfeef707327b1f451e1056a18d307ea287176
GyanXspy <gyanaranjanpatra780@gmail.com>
2025-08-13 20:41:08 +0530

    html

---
+++ myapp/myapp/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html') 
+
+
+def about(request):
+    return HttpResponse("Hello world about")
+
+
+def contact(request):
+    return HttpResponse("Hello world contact")
